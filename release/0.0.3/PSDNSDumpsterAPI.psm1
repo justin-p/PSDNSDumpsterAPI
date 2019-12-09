4312a0fdfd7a2ba31ca2ebe0b36c9f1966415ca9
Justin-p <justin-p@users.noreply.github.com>
2019-12-09 20:49:54 +0100

    replaced parsing with html agility pack

--- release/0.0.3/PSDNSDumpsterAPI.psm1
+++ release/0.0.3/PSDNSDumpsterAPI.psm1
@@ -14,9 +14,9 @@
 function Convert-PSDNSDumpsterAPIDomainInfo {
     <#
     .SYNOPSIS
-    Parse the output from DNSDumpster to a PSObject.
+    Create a parsed output from DNSDumpster.
     .DESCRIPTION
-    Parse the output from DNSDumpster to a PSObject. This functions expects a PSObject created by Get-PSDNSDumpsterAPIDomainInfo.
+    Create a parsed output from DNSDumpster. This functions expects a PSObject created by Get-PSDNSDumpsterAPIDomainInfo.
     .PARAMETER DomainName
     The name of the domain.
     .PARAMETER ScanResults
@@ -44,12 +44,8 @@ function Convert-PSDNSDumpsterAPIDomainInfo {
         Write-Verbose "$($FunctionName) - Begin."
         Try {
             Try {
-                $DNSObject   = @()
-                $MXObject    = @()
-                $TXTObject   = @()
-                $HostObject  = @()
                 #$ExcelObject = @()
-            } Catch {
+                } Catch {
                 Write-Error "$($FunctionName) - $PSItem"
             }
         } Catch {
@@ -58,71 +54,28 @@ function Convert-PSDNSDumpsterAPIDomainInfo {
     } Process {
         Write-Verbose "$($FunctionName) - Processing '$DomainName'"
         Try {
+            $doc = New-Object HtmlAgilityPack.HtmlDocument
+            $doc.LoadHtml($ScanResults)
+            $tables = $doc.DocumentNode.SelectNodes("//table")
             Try {
-                Write-Debug "$($FunctionName) - Extract info out of the web request"
-                $tables    = @($ScanResults.ParsedHtml.getElementsByTagName("TABLE"))
-                #$a         = @($ScanResults.ParsedHtml.getElementsByTagName("A"))
-                $DNSRows   = $($tables[0]).Rows
-                $MXRows    = $($tables[1]).Rows
-                $TXTRows   = $($tables[2]).Rows
-                $HostRows  = $($tables[3]).Rows
-                #$ExcelHref = $a[24]
-            }
-            Catch {
-                Write-Error "$($FunctionName) - Unable to parse '`$ScanResults' - $PSItem"
-            }
-            Write-Debug "$($FunctionName) - Go through all of the rows we defined."
-            Try {
-                ForEach ($row in $DNSRows) {
-                    $cells = @($row.Cells)
-                    $resultObject = [Ordered] @{ }
-                    $resultObject["nameserver"] = @(""  + $cells[0].InnerText).Trim().TrimEnd('.')
-                    $resultObject["ip"]         = @(("" + $cells[1].InnerText).Trim().split([Environment]::NewLine) | Where-Object {$_ -notmatch "^$"})[0]
-                    $resultObject["reversedns"] = @(("" + $cells[1].InnerText).Trim().split([Environment]::NewLine) | Where-Object {$_ -notmatch "^$"})[1]
-                    $resultObject["asn"]        = @(("" + $cells[2].InnerText).Trim().split([Environment]::NewLine) | Where-Object {$_ -notmatch "^$"})[0]
-                    $resultObject["country"]    = @(("" + $cells[2].InnerText).Trim().split([Environment]::NewLine) | Where-Object {$_ -notmatch "^$"})[1]
-                    $DNSObject +=[PSCustomObject] $resultObject
-                }
+                $DNSObject = Get-PSDNSDumpsterAPIResultsFromTable -Table $tables[0] -dns
             } Catch {
-                Write-Error "$($FunctionName) - Unable to parse '`$DNSRows'- $PSItem"
+                Write-Error "$($FunctionName) - Unable to parse DNS Table - $PSItem"
             }
             Try {
-                ForEach ($row in $MXRows) {
-                    $cells = @($row.Cells)
-                    $resultObject = [Ordered] @{ }
-                    $resultObject["priority"]   = @((""  + $cells[0].InnerText).Trim().split(" ") | Where-Object {$_ -notmatch "^$"})[0]
-                    $resultObject["mx"]         = @((("" + $cells[0].InnerText).Trim().split(" ") | Where-Object {$_ -notmatch "^$"})[1]).TrimEnd('.')
-                    $resultObject["ip"]         = @((""  + $cells[1].InnerText).Trim().split([Environment]::NewLine) | Where-Object {$_ -notmatch "^$"})[0]
-                    $resultObject["reversedns"] = @((""  + $cells[1].InnerText).Trim().split([Environment]::NewLine) | Where-Object {$_ -notmatch "^$"})[1]
-                    $resultObject["asn"]        = @((""  + $cells[2].InnerText).Trim().split([Environment]::NewLine) | Where-Object {$_ -notmatch "^$"})[0]
-                    $resultObject["country"]    = @((""  + $cells[2].InnerText).Trim().split([Environment]::NewLine) | Where-Object {$_ -notmatch "^$"})[1]
-                    $MXObject +=[PSCustomObject] $resultObject
-                }
+                $MXObject = Get-PSDNSDumpsterAPIResultsFromTable -Table $tables[1] -mx
             } Catch {
-                Write-Error "$($FunctionName) - Unable to parse '`$MXRows'- $PSItem"
+                Write-Error "$($FunctionName) - Unable to parse MX Table - $PSItem"
             }
             Try {
-                ForEach ($row in $TXTRows) {
-                    $TXTObject +=$row.InnerText
-                }
-                $TXTObject = New-Object PSObject -property @{TXTRecords=$TXTObject}
+                $TXTObject = Get-PSDNSDumpsterAPIResultsFromTable -Table $tables[2] -txt
             } Catch {
-                Write-Error "$($FunctionName) - Unable to parse '`$TXTRows'- $PSItem"
+                Write-Error "$($FunctionName) - Unable to parse TXT Table - $PSItem"
             }
             Try {
-                ForEach ($row in $HostRows) {
-                    $cells = @($row.Cells)
-                    $resultObject = [Ordered] @{ }
-                    $resultObject["host"]       = @(("" + $cells[0].InnerText).Trim().split([Environment]::NewLine) | Where-Object {$_ -notmatch "^$"})[0]
-                    $resultObject["services"]   = @(("" + $cells[0].InnerText).Trim().split([Environment]::NewLine) | Where-Object {$_ -notmatch "^$"})[(2..(("" + $cells[0].InnerText).Trim().split([Environment]::NewLine) | Where-Object {$_ -notmatch "^$"}).count)]
-                    $resultObject["ip"]         = @(("" + $cells[1].InnerText).Trim().split([Environment]::NewLine) | Where-Object {$_ -notmatch "^$"})[0]
-                    $resultObject["reversedns"] = @(("" + $cells[1].InnerText).Trim().split([Environment]::NewLine) | Where-Object {$_ -notmatch "^$"})[1]
-                    $resultObject["asn"]        = @(("" + $cells[2].InnerText).Trim().split([Environment]::NewLine) | Where-Object {$_ -notmatch "^$"})[0]
-                    $resultObject["country"]    = @(("" + $cells[2].InnerText).Trim().split([Environment]::NewLine) | Where-Object {$_ -notmatch "^$"})[1]
-                    $HostObject +=[PSCustomObject] $resultObject
-                }
+                $HostObject = Get-PSDNSDumpsterAPIResultsFromTable -Table $tables[3] -hosts
             } Catch {
-                Write-Error "$($FunctionName) - Unable to parse '`$HostRows'- $PSItem"
+                Write-Error "$($FunctionName) - Unable to parse Hosts Table - $PSItem"
             }
             Try {
                 $ImageObject = Get-PSDNSDumpsterAPIImage -URL $("https://dnsdumpster.com/static/map/" + $DomainName + ".png")
@@ -140,7 +93,7 @@ function Convert-PSDNSDumpsterAPIDomainInfo {
     } End {
         Write-Verbose "$($FunctionName) - End."
             Return $(New-Object psobject -Property @{DomainName=$DomainName;DNSDumpsterObject=@{DNS=$DNSObject;MX=$MXObject;TXT=$TXTObject;Host=$HostObject;Image=$ImageObject;};DNSDumpsterSession=$DNSDumpsterSession;})
-        }
+    }
 }
 
 function Get-CallerPreference {
@@ -444,6 +397,140 @@ Function Get-PSDNSDumpsterAPIImage {
         } Catch {
             $PSCmdlet.ThrowTerminatingError($PSItem)
         }
+    }
+    End {
+        Write-Verbose "$($FunctionName) - End."
+        Try {
+            Try {
+                Return $OutputObject
+            } Catch {
+                Write-Error "$($FunctionName) - $PSItem"
+            }
+        } Catch {
+            $PSCmdlet.ThrowTerminatingError($PSItem)
+        }
+    }
+}
+
+Function Get-PSDNSDumpsterAPIResultsFromTable {
+    <#
+    .SYNOPSIS
+    Parse the tables.
+    .DESCRIPTION
+    Parse the tables.
+    .PARAMETER table
+    Table to parse.
+    .PARAMETER dns
+    Parse as DNS table.
+    .PARAMETER mx
+    Parse as MX table.
+    .PARAMETER txt
+    Parse as txt table.
+    .PARAMETER hosts
+    Parse as hosts table.
+    .EXAMPLE
+    Get-PSDNSDumpsterAPIResultsFromTable -Table $tables[0] -dns
+    .NOTES
+    Author: Justin Perdok, https://justin-p.me.
+    Project: https://github.com/justin-p/PSDNSDumpsterAPI
+    #>
+    [CmdletBinding()]
+    Param(
+        [parameter(Mandatory = $true)]
+        [PSObject]$table,
+        [Switch]$dns,
+        [Switch]$mx,
+        [Switch]$txt,
+        [Switch]$hosts
+    )
+    Begin {
+        if ($script:ThisModuleLoaded -eq $true) {
+            Get-CallerPreference -Cmdlet $PSCmdlet -SessionState $ExecutionContext.SessionState
+        }
+        $FunctionName = $MyInvocation.MyCommand.Name
+        Write-Verbose "$($FunctionName) - Begin."
+        Try {
+            Try {
+                $trs = $table.SelectNodes($($table.xpath + "/tr"))
+                $IPPattern = [Regex]::new('[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}')
+                $resultObject = [Ordered] @{ }
+                $OutputObject = @()
+            } Catch {
+                Write-Error "$($FunctionName) - $PSItem"
+            }
+        } Catch {
+            $PSCmdlet.ThrowTerminatingError($PSItem)
+        }
+    }
+    Process{
+        Write-Verbose "$($FunctionName) - Processing $URL"
+        Try {
+            Try {
+                ForEach ($tr in $trs) {
+                    Try {
+                        $tds = $tr.SelectNodes($($tr.xpath + "/td"))
+                        $ip = $IPPattern.Matches($tds[1].InnerHtml).value
+                        $domain = ($tds[0].InnerText.Trim().split([Environment]::NewLine) | Where-Object {$_ -notmatch "^$"})
+                        $reverse_dns = $tds[1].InnerText.Replace($IPPattern.Matches($tds[1].InnerHtml).value, '')
+                        $country = $tds[2].InnerHTML.split('>').Split('<')[4]
+                        $asn = $tds[2].InnerText.Replace($country,'')
+                    } Catch {
+                        Write-Debug "$($FunctionName) - $PSItem"
+                    }
+                    if ($dns) {
+                        $resultObject['nameserver'] = $domain
+                        $resultObject['ip'] = $ip
+                        $resultObject['reversedns'] = $reverse_dns
+                        $resultObject['asn'] = $asn
+                        $resultObject['country'] = $country
+                        $OutputObject +=[PSCustomObject] $resultObject
+                    }
+                    ElseIf ($mx) {
+                        $resultObject['priority'] = $domain.split(' ')[0];
+                        $resultObject['mx'] = $domain.split(' ')[1];
+                        $resultObject['ip'] = $ip;
+                        $resultObject['reversedns'] = $reverse_dns;
+                        $resultObject['asn'] = $asn;
+                        $resultObject['country'] = $country;
+                        $OutputObject +=[PSCustomObject] $resultObject
+                    }
+                    ElseIf($txt) {
+                        ForEach($td in $tds) {
+                            $resultObject['TXTRecords'] = $td.InnerText.Replace("&quot;","`"")
+                            $OutputObject +=[PSCustomObject] $resultObject
+                        }
+                    }
+                    ElseIf($Hosts) {
+                        if ($domain.getType().Name -ne 'string') {
+                            $services    = @(($domain.Trim().split([Environment]::NewLine) | Where-Object {$_ -notmatch "^$"}))[(1..$($domain.Trim().split([Environment]::NewLine.Trim()) | Where-Object {$_ -notmatch "^$"}).count)]
+                            $count = 1
+                            $services = ForEach ($service in $services) {
+                                if ($count % 2 -eq 1 ) {
+                                    $service + " " + $services[$count]
+                                }
+                                $count++
+                            }
+                            $host_domain = $domain[0]
+                        } Else {
+                            $host_domain = $domain
+                            $services = $null
+                        }
+                        $resultObject['host'] = $host_domain ;
+                        $resultObject['services'] = $services;
+                        $resultObject['ip'] = $ip;
+                        $resultObject['reversedns'] = $reverse_dns;
+                        $resultObject['asn'] = $asn;
+                        $resultObject['country'] = $country;
+                        $OutputObject +=[PSCustomObject] $resultObject
+                    }
+                }
+            } Catch {
+                Write-Error "$($FunctionName) - $PSItem"
+            }
+        } Catch {
+            $PSCmdlet.ThrowTerminatingError($PSItem)
+        }
+
     }
     End {
         Write-Verbose "$($FunctionName) - End."
